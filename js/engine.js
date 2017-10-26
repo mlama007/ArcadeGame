d36e8d9c88fb643acc7c4ab8da1ad65bec28d309
Maria Lamardo <marialamardo@outlook.com>
2017-10-25 23:20:45 -0400

    First commit

--- js/engine.js
+++ js/engine.js
@@ -9,9 +9,8 @@
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
  *
- * This engine is available globally via the Engine variable and it also makes
- * the canvas' context (ctx) object globally available to make writing app.js
- * a little simpler to work with.
+ * This engine makes the canvas' context (ctx) object globally available to make 
+ * writing app.js a little simpler to work with.
  */
 
 var Engine = (function(global) {
@@ -171,7 +170,11 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png'
     ]);
     Resources.onReady(init);
 
